713f99c165985da52fb901a06cea2181fcb86c1b
Nur Ariffin Mohd Zin <nurariffinmohdzin@gmail.com>
2024-11-05 07:52:29 +0800

    first commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/components/sidebar.blade.php -->
+<div class="col-span-2 p-4">
+    <!-- Sidebar content -->
+    <ul class="menu bg-white rounded-box w-50">
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+        <li><a>Item 3</a></li>
+    </ul>
+</div>
